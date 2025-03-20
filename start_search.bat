d37d02f0aaa162a02ad80b860baed99bf61169df
Daniele Iorio <81297468+IorioD@users.noreply.github.com>
2025-03-21 00:59:17 +0100

    Update start_search.bat

--- start_search.bat
+++ start_search.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Esegue il file JAR generato
-echo Avvio applicazione...
+echo Starting application...
 call java -jar .\target\pdf-search.jar
 if %ERRORLEVEL% NEQ 0 (
     echo Errore durante l'esecuzione dell'applicazione.
